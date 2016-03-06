c4c154dda90cbb2e50aea7879f9013b51cb99bbe
Mateusz Kuźnik <mateusz.kuznik@evostudio.pl>
2016-03-06 14:10:43 +0100

    add view for results

--- podglad/script.js
+++ podglad/script.js
@@ -1,17 +1,17 @@
 /*var canvasBefore = document.getElementById("canvasBefore");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-//centrum od gory, centrum od lewej, promien
-ctx.arc(10, 10, 10, 0, 2 * Math.PI);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-
-var ctx2 = c.getContext("2d");
-ctx2.beginPath();
-//od gory, od lewej, wielkość
-ctx2.arc(30, 10, 10, 0, 2 * Math.PI);
-ctx2.fillStyle = "#FF0000";
-ctx2.stroke();*/
+ var ctx = c.getContext("2d");
+ ctx.beginPath();
+ //centrum od gory, centrum od lewej, promien
+ ctx.arc(10, 10, 10, 0, 2 * Math.PI);
+ ctx.fillStyle = "#FF0000";
+ ctx.fill();
+ 
+ var ctx2 = c.getContext("2d");
+ ctx2.beginPath();
+ //od gory, od lewej, wielkość
+ ctx2.arc(30, 10, 10, 0, 2 * Math.PI);
+ ctx2.fillStyle = "#FF0000";
+ ctx2.stroke();*/
 
 var painter = {
     settings: {
@@ -44,18 +44,18 @@ var painter = {
         var previousColor;
         data.forEach(function (e, i) {
             var randomColor = (i % 2 == 0) ? painter.randomColor() : previousColor,
-                point = painter.raw.canvas.getContext("2d"),
-                radius = e.type ? painter.settings.radius / 1.5 : painter.settings.radius,
-                x = radius + e.longitude,
-                y = radius + e.latitude;
+                    point = painter.raw.canvas.getContext("2d"),
+                    radius = e.type ? painter.settings.radius / 1.5 : painter.settings.radius,
+                    x = radius + e.longitude,
+                    y = radius + e.latitude;
             point.beginPath();
             point.arc(x, y, radius, 0, 2 * Math.PI);
             point.fillStyle = color ? color : randomColor;
             point.fill();
             if (e.type && e.id % 2 == 1) {
                 var line = painter.raw.canvas.getContext("2d");
-                line.moveTo(data[i - 1].longitude+radius, data[i - 1].latitude+radius);
-                line.lineTo(e.longitude+radius, e.latitude+radius);
+                line.moveTo(data[i - 1].longitude + radius, data[i - 1].latitude + radius);
+                line.lineTo(e.longitude + radius, e.latitude + radius);
                 line.strokeStyle = randomColor;
                 line.stroke();
             }
@@ -66,10 +66,9 @@ var painter = {
                 pointText.fillText(e.task ? e.task : e.id, x - (radius / 2), y + (radius / 3));
             }
             previousColor = randomColor;
-            
+
         })
     },
-
     get: function (url, callback) {
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.onreadystatechange = function () {
@@ -84,6 +83,85 @@ var painter = {
     }
 };
 
+var painterAfter = {
+    settings: {
+        radius: 8,
+    },
+    raw: {
+        url: "../algorytm/result.json",
+        proceed: function (data) {
+            painterAfter.raw.paths = data;
+            painterAfter.raw.draw();
+        },
+        canvas: document.getElementById("canvasAfter"),
+        paths: [],
+        draw: function () {
+            painterAfter.draw(this.paths);
+        },
+    },
+    randomColor: function () {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    },
+    draw: function (data) {
+        var previousColor;
+        data.forEach(function (el, j) {
+            var randomColor = painterAfter.randomColor();
+            el.forEach(function (e, i) {
+                console.log(e);
+
+                var point = painterAfter.raw.canvas.getContext("2d"),
+                        radius = painterAfter.settings.radius,
+                        x = radius + e.longitude,
+                        y = radius + e.latitude;
+                point.beginPath();
+                point.arc(x, y, radius, 0, 2 * Math.PI);
+                point.fillStyle = i == 0 ? "black" : randomColor;
+                point.fill();
+                if (e.type == "pickup") {
+                    var point = painterAfter.raw.canvas.getContext("2d"),
+                            radius =6,
+                            x = radius + e.longitude,
+                            y = radius + e.latitude;
+                    point.beginPath();
+                    point.arc(x+2, y+2, radius, 0, 2 * Math.PI);
+                    point.fillStyle = "white";
+                    point.fill();
+                }
+                if (i > 0) {
+                    var line = painterAfter.raw.canvas.getContext("2d");
+                    line.moveTo(data[j][i - 1].longitude + radius, data[j][i - 1].latitude + radius);
+                    line.lineTo(e.longitude + radius, e.latitude + radius);
+                    line.strokeStyle = randomColor;
+                    line.stroke();
+                }
+                var pointText = painterAfter.raw.canvas.getContext("2d");
+                pointText.font = "16px Arial";
+                pointText.fillStyle = 'black';
+                pointText.fillText(typeof (e.task) != "undefined" ? e.task : e.id, x - (radius / 2), y - 13 + (radius / 3));
+            });
+        })
+    },
+    get: function (url, callback) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                callback(JSON.parse(xmlHttp.responseText));
+        }
+        xmlHttp.open("GET", url, true);
+        xmlHttp.send(null);
+    },
+    init: function () {
+        this.get(this.raw.url, this.raw.proceed);
+    }
+};
+
+
 window.onload = function () {
     painter.init();
+    painterAfter.init();
 }
